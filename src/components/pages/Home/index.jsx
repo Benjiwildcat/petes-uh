5482624092eb6d9bd7b846636d516b1e04661a0b
unknown <thomasjjohnson9311@gmail.com>
2024-04-20 21:21:08 -0500

    border test

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -4,7 +4,9 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <div class="container mx-auto">This is container with a border.</div>
+      <div class="container mx-auto border-2 border-red-300">
+        This is container with a border.
+      </div>
     </div>
   );
 }
