1c7b576be0ae9096ca04e5d28646e8ecf7b8ef8b
benjiwildcat <benjiwildcat@gmail.com>
2024-04-20 22:08:46 -0500

    giving pages context

--- src/components/pages/Locations/index.jsx
+++ src/components/pages/Locations/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Locations() {
   return (
     <div>
-      <h1>Locations Page</h1>
+      <h1>Try one of our three beautiful stores Today!</h1>
 
       <p>Central</p>
       <text>111-1111</text>
