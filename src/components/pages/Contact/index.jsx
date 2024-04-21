523b729179b563ab0728969784b5538e4aa1db7a
benjiwildcat <benjiwildcat@gmail.com>
2024-04-20 22:04:34 -0500

    adding context to pages

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -4,6 +4,8 @@ function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
+
+      <p>P. SHerman 42 Wallaby Way, Sydney /  111-1111</p>
     </div>
   );
 }
--- src/components/pages/Locations/index.jsx
+++ src/components/pages/Locations/index.jsx
@@ -4,6 +4,13 @@ function Locations() {
   return (
     <div>
       <h1>Locations Page</h1>
+
+      <p>Central</p>
+      <text>111-1111</text>
+      <p>North</p>
+      <text>111-1112</text>
+      <p>South</p>
+      <text>111-1113</text>
     </div>
   );
 }
